2431f248ed92b617cbd9ea486e55d22ea0e1fbc8
Parminder Singh <paramsinghvc@gmail.com>
2016-04-17 23:48:58 +0530

    Init Commit

---
+++ app.ts
@@ -0,0 +1,5 @@
+// import {HTTP_BINDINGS} from 'angular2/http';
+import {bootstrap} from 'angular2/platform/browser';
+import {App} from './src/main';
+
+bootstrap(App);
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    template : `Hello There`,
+    selector : 'my-app'
+})
+export class App{
+    constructor(){
+
+    }
+}
